8646512ee0f059c8614b9b4c01e8eb566c1ad338
Harsha01022023 <shooliniinfotech20@gmail.com>
2025-07-03 15:34:14 +0530

    Add files via upload

---
+++ pgsql.sql
@@ -0,0 +1,9 @@
+project/
+├── public/
+│   ├── dashboard.html
+│   ├── style.css
+├── files/
+│   ├── Hourly Count.xlsx
+│   ├── Monthly Work Report.xlsx
+│   ├── Spanish Outputs.xlsx
+│   ├── Attendance.xlsx
